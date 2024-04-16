4c54f4260b97c2486e6867ef014e913bd20d1440
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-04-16 16:47:37 +0200

    feat: esercizio terminato

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             //array cibi
-            string[] cibiPreferiti = { "Pizza", "Patatine", "Sushi", "Pasta", "Cozze", "Arrosto", "Insalata" };
+            string[] cibiPreferiti = { "Pizza", "Patatine", "Sushi", "Pasta", "Cozze", "Arrosto", "Rollè di Patate", "Insalata" };
 
             //lunghezza array
             Console.WriteLine($"La lunghezza dell'array è: {cibiPreferiti.Length}");
@@ -17,7 +17,22 @@
                 Console.WriteLine($"{i + 1} = {cibiPreferiti[i]}");
             }
 
-           
+            //cibo preferito in assoluto
+            Console.WriteLine($"Cibo preferito in assoluto: {cibiPreferiti[0]}");
+
+            //cibo preferito ma non troppo
+            int ciboUltimo = cibiPreferiti.Length - 1;
+            Console.WriteLine($"Cibo preferito ma non troppo: {cibiPreferiti[ciboUltimo]}");
+
+            //cibo preferito intermedio
+            int ciboIntermedio = cibiPreferiti.Length /2;
+
+            //condizione per cibo intermedio
+            if (cibiPreferiti.Length % 2 == 0)
+            {
+                Console.WriteLine($"Cibo preferito intermedio 1: {cibiPreferiti[ciboIntermedio - 1]}");
+                Console.WriteLine($"Cibo preferito intermedio 2: {cibiPreferiti[ciboIntermedio]}");
+            }
         }
     }
 }
